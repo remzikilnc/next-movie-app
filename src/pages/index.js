19256bcc89f2830d181e166aecc0a6d8fbe108f4
Remzi Kılınç <remzikilnc46@gmail.com>
2023-11-11 09:59:37 +0300

    Revert "Initialized Tailwind CSS and made some appearance-related changes"
    This reverts commit cc6e831ec819c8df0716a4f92b81d0e885d26371.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import {useState} from "react";
 import axios from "axios";
 import Card from "@/components/card";
-import {BsSearch} from "react-icons/bs";
 
 const options = {
-    method: 'GET', headers: {
+    method: 'GET',
+    headers: {
         accept: 'application/json',
         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MTEyNWUxY2ZkM2FjYzM0M2UwMjc2NjcyZDM5ODk0YiIsInN1YiI6IjYwOTgyYzRiMDkyOWY2MDAzYWRlZDM1ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Hx8emELviZA_xteYrcflxshQptM13PcHkqWQDyL8n4k'
     }
@@ -23,24 +23,19 @@ export default function Home() {
             .catch(err => console.error(err));
     }
 
-    return (<>
-        <header className="w-full flex justify-center items-center bg-gray-900 py-4 bg-opacity-80">
-            <section className="flex">
-                <div className="flex justify-center items-center bg-gray-800 rounded px-1 py-1 gap-x-2">
-                    <BsSearch className="text-white ml-1"/>
-                    <input
-                        className="bg-transparent placeholder:text-gray-300 focus:ring-0 focus:outline-none text-white"
-                        onChange={(e) => setSearch(e.target.value)} type="text" placeholder="Yazmaya başla.."/>
-                </div>
-                <button className="text-white bg-purple-800 rounded mx-4 px-2 text-sm" onClick={handleClick}>Arama
-                </button>
-            </section>
-
-        </header>
-        <section className="p-3">
-            <ul className="grid grid-cols-8 gap-4">
-                {movies.map(movie => (<Card key={movie.id} movie={movie}/>))}
+  return (
+    <>
+        <section>
+            <input onChange={(e) => setSearch(e.target.value)} type="text" placeholder="Bir şeyler yaz.."/>
+            <button onClick={handleClick}>Ara</button>
+        </section>
+        <section>
+            <ul className="card-listing">
+            {movies.map(movie => (
+              <Card key={movie.id} movie={movie} />
+            ))}
             </ul>
         </section>
-    </>)
+    </>
+  )
 }
--- src/pages/movie/[id]/index.jsx
+++ src/pages/movie/[id]/index.jsx
@@ -21,40 +21,14 @@ const Index = () => {
         }
     }, [id]);
 
-    console.log(movie)
+
     if (movie === null) {
         return <p>Loading...</p>
     }
     return (
-        <div className="flex justify-center items-center mt-8">
-           <div className="flex justify-center flex-col items-center gap-y-4">
-               <div className="relative justify-center items-center flex flex-col p-8 gap-y-1">
-                   <div className="justify-center flex items-center flex-col bg-stone-800 p-2 w-full rounded rounded-b-none bg-opacity-40">
-                       <h1 className="text-white text-xl font-semibold">{movie.title}</h1>
-                   </div>
-                   <img className="rounded rounded-b-none" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="poster"/>
-                   <div className="flex justify-center items-center gap-x-4 bg-stone-800 p-2 w-full rounded rounded-b-none bg-opacity-60">
-                       <p className="text-white text-sm font-semibold mt-4">{movie.overview}</p>
-                   </div>
-                   <div className="flex justify-center items-center gap-x-4 bg-stone-800 p-2 w-full rounded rounded-b-none bg-opacity-50">
-                       <p className="text-white text-sm font-semibold">Rating: <span className="text-yellow-400">{movie.vote_average}</span></p>
-                   </div>
-                   <div className="w-full">
-                       <div className="justify-center flex items-center flex-col bg-stone-800 p-2 w-full rounded rounded-b-none bg-opacity-40">
-                           <div className="flex justify-center items-center gap-x-4">
-                               {movie.genres.map(genre => (
-                                   <p className="text-white text-sm font-normal">{genre.name}</p>
-                               ))}
-                           </div>
-                       </div>
-                   </div>
-                   <div className="bg-opacity-10 bg-white w-full h-full absolute top-0 -z-10 blur-sm rounded"/>
-               </div>
-
-           </div>
-            <div className="absolute opacity-40 blur-2xl -z-10 w-full h-full">
-                <img className="w-full h-full" src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path ?? movie.poster_path}`} alt="backdrop"/>
-            </div>
+        <div>
+            <h1>{movie.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="poster"/>
         </div>
     );
 };
